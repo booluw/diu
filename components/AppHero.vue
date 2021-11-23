0c4e1b58b68b500e1801f4c4353793c41d274e1a
= <=>
2021-11-23 10:36:29 +0100

    :star: Completed AppHero component.

---
+++ components/AppHero.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="jumbotron">
+    <h1 class="heading heading--super">
+      david
+      <br />
+      utulor
+    </h1>
+    <div class="row m-0 align-items-end">
+      <div class="col-md-5 p-0">
+        <img src="~/static/img/David-Utulor.png" class="jumbotron__img" alt="David Utulor" />
+      </div>
+      <div class="col-md-5 p-0">
+        <div class="jumbotron__text">
+          <h2 class="jumbotron__heading heading">David Ikechuckwu Utulor</h2>
+          <h3 class="jumbotron__heading heading heading--sub">
+            <abbr title="Chief Executive Officer">CEO</abbr>, Diatheke Empire.
+          </h3>
+          <p class="jumbotron__paragraph">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga non vitae quae molestias. Blanditiis excepturi magni omnis voluptate earum. Cupiditate perspiciatis labore dicta assumenda expedita provident ad tempora consequatur. Sint?
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppHero'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <section>
     <app-header />
+    <app-hero />
   </section>
 </template>
 
 <script>
 import AppHeader from '~/components/AppHeader.vue'
+import AppHero from '~/components/AppHero.vue'
 
 export default {
-  components: { AppHeader },
+  components: { AppHeader, AppHero },
 }
 </script>
