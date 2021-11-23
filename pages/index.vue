03d3a58d80007bfcc5eebeeb1746a9da72f1feb7
= <=>
2021-11-23 11:59:19 +0100

    :star: Completed Sections and Homepage.

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,23 @@
   <section>
     <app-header />
     <app-hero />
+    <section class="section">
+      <h2 class="section__heading heading">Section header</h2>
+      <p class="section__paragraph paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis maxime blanditiis sed itaque, explicabo temporibus, sint nesciunt distinctio nulla delectus facere hic illum iste asperiores, rerum ipsum ad ipsa nobis porro? Recusandae laboriosam necessitatibus placeat iure, voluptas qui quos voluptate illo, rem cum quia, nulla sed non dolores quasi voluptatibus eligendi! Id tempora laudantium quos at sunt exercitationem dicta consectetur? Sunt alias ipsa quasi fuga incidunt velit corporis autem, sequi cupiditate dolor corrupti at inventore sapiente in minus porro debitis ut itaque id? Tempora, hic. Dolorem, laborum soluta assumenda vero dolor fugit tenetur labore eius dolores quasi suscipit vel blanditiis. Aliquam nostrum incidunt autem veniam possimus. Quos voluptas, dolorum fugiat cum quae ipsam amet nesciunt, facere officia praesentium similique tenetur?
+      </p>
+    </section>
+    <section class="section section--bg-milk">
+      <h2 class="section__heading heading">Section heading</h2>
+      <p class="section__paragraph paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, obcaecati. Enim magni molestias ad aut doloremque aperiam ea necessitatibus sit quasi vel maxime dolor officia unde odit, reiciendis quo eveniet! Veritatis perferendis dolorum est eum porro quae illo expedita, natus sint sed totam hic, reiciendis unde! Adipisci voluptates architecto exercitationem doloribus excepturi. Dolorem aliquid pariatur consequatur mollitia architecto, ipsam deserunt, quam, vitae delectus reprehenderit facilis optio a fugit facere velit beatae. Architecto necessitatibus, similique nemo dolor officia accusantium doloremque libero nostrum voluptatem amet laudantium illum rerum quod quis animi id tenetur aliquam numquam possimus beatae fugit quae a enim natus? Consequatur mollitia sint voluptas voluptates eos eligendi aspernatur itaque velit totam nisi. Fuga, consequatur incidunt? Quas labore soluta aperiam quam cum aut eos quidem itaque, quaerat ratione est officia quis!
+      </p>
+      <br /><br />
+      <h3 class="section__heading heading heading--sub">Hello world.</h3>
+      <p class="section__paragraph paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, obcaecati. Enim magni molestias ad aut doloremque aperiam ea necessitatibus sit quasi vel maxime dolor officia unde odit, reiciendis quo eveniet! Veritatis perferendis dolorum est eum porro quae illo expedita, natus sint sed totam hic, reiciendis unde! Adipisci voluptates architecto exercitationem doloribus excepturi. Dolorem aliquid pariatur consequatur mollitia architecto, ipsam deserunt, quam, vitae delectus reprehenderit facilis optio a fugit facere velit beatae. Architecto necessitatibus, similique nemo dolor officia accusantium doloremque libero nostrum voluptatem amet laudantium illum rerum quod quis animi id tenetur aliquam numquam possimus beatae fugit quae a enim natus? Consequatur mollitia sint voluptas voluptates eos eligendi aspernatur itaque velit totam nisi. Fuga, consequatur incidunt? Quas labore soluta aperiam quam cum aut eos quidem itaque, quaerat ratione est officia quis!
+      </p>
+    </section>
   </section>
 </template>
 
